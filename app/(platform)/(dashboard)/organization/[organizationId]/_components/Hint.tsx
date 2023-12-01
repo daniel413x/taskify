2d22eba5bd396410d015ef6030416f6b3b171f06
Daniel Maramba <daniel413x@gmail.com>
2023-12-01 14:49:49 -0500

    fix Hint tooltip issues

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/Hint.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/Hint.tsx
@@ -3,6 +3,7 @@
 import {
   Tooltip, TooltipContent, TooltipProvider, TooltipTrigger,
 } from '@/components/ui/common/shadcn/tooltip';
+import useHasMounted from '@/lib/hooks/useHasMounted';
 import { Children } from '@/lib/types';
 
 interface HintProps {
@@ -17,22 +18,26 @@ const Hint = ({
   desc,
   side = 'bottom',
   sideOffset = 0,
-}: HintProps) => (
-  <TooltipProvider>
-    <Tooltip delayDuration={0}>
-      <TooltipTrigger>
-        {children}
-      </TooltipTrigger>
-      <TooltipContent
-        sideOffset={sideOffset}
-        side={side}
-        className="text-xs max-w-[220px] break-words"
-      >
-        {desc}
-      </TooltipContent>
-    </Tooltip>
-
-  </TooltipProvider>
-);
+}: HintProps) => {
+  if (!useHasMounted()) {
+    return;
+  }
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>
+          {children}
+        </TooltipTrigger>
+        <TooltipContent
+          sideOffset={sideOffset}
+          side={side}
+          className="text-xs max-w-[220px] break-words whitespace-normal"
+        >
+          {desc}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
 
 export default Hint;
